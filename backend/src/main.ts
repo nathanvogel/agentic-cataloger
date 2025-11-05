ecd7473a8391beb945337587895162c8f75b2438
Nathan Vogel <nathan@nathanvogel.com>
2025-11-05 21:57:29 +0100

    chore: test bad format again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  console.log("Bootstrapping 3...");
+  console.log("Bootstrapping 4...");
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3010);
 }
