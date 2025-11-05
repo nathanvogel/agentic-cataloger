2d1825e4ff903da35668ca7ed85c16efdfa0f9b4
Nathan Vogel <nathan@nathanvogel.com>
2025-11-05 21:22:49 +0100

    chore: yarn format

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LLMModule } from './llm/llm.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LLMModule } from "./llm/llm.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      envFilePath: ".env",
     }),
     LLMModule,
   ],
