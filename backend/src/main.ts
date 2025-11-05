84737da77fa337cabbb623af851bf492e0ba1b74
Nathan Vogel <nathan@nathanvogel.com>
2025-11-05 21:52:00 +0100

    chore: test pre-commit hook on badly formatted file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+  console.log("Bootstrapping...")
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3010);
 }
